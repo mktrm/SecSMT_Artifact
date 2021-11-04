5dd1d2a09bc1f25309f521bb7cbe8a4030c1d3e6
Kazem Taram <mtaram@ucsd.edu>
2021-11-04 12:39:25 -0700

    improve scripts for running the JS benchamrks

--- mitigations/scripts/browserlike/run.sh
+++ mitigations/scripts/browserlike/run.sh
@@ -13,42 +13,27 @@ module load git
 module load gcc-6.3.0
 module load python3.6.2
 
-HOME=/u/xr5ry/
-PROJ="/u/mtaram/"
-bin1="$HOME/src/wolfssl-4.5.0/wolfcrypt/benchmark/benchmark"
+BASE=`dirname "$0"`
+PROJ="$BASE/../"
+WORKLOAD="$PROJ/workloads/browserlike_flat/"
+bin1="$WORKLOAD/wolfssl-4.5.0/wolfcrypt/benchmark/benchmark"
 args1=$2
-bin2="$HOME/src/duktape-2.6.0/duk"
+bin2="$WORKLOAD/duktape-2.6.0/duk"
 args2=$3
-args2list="3d-raytrace.js controlflow-recursive.js math-cordic.js regexp-dna.js string-base64.js"
+
 
 case "$1" in
   
   shared)
-#    outdir=shared
     simpots=''
     ;;
   partitioned)
-#    outdir=partitioned
     simopts='--smtFetchPolicy=StrictRoundRobin --smtBTBPolicy=PartitionedSMTPolicy --smtCachePolicy=PartitionedSMTPolicy --smtROBPolicy=PartitionedSMTPolicy --smtIQPolicy=PartitionedSMTPolicy --smtTLBPolicy=PartitionedSMTPolicy --smtPhysRegPolicy=PartitionedSMTPolicy --smtSQPolicy=PartitionedSMTPolicy --smtLQPolicy=PartitionedSMTPolicy --smtCommitPolicy=StrictRoundRobin --smtIssuePolicy=MultiplexingFUs'
     ;;
   adaptive)
-#    outdir=adaptive
     simopts='--smtFetchPolicy=Adaptive --smtBTBPolicy=AdaptiveSMTPolicy --smtCachePolicy=AdaptiveSMTPolicy --smtROBPolicy=AdaptiveSMTPolicy --smtIQPolicy=AdaptiveSMTPolicy --smtTLBPolicy=AdaptiveSMTPolicy --smtPhysRegPolicy=AdaptiveSMTPolicy  --smtSQPolicy=AdaptiveSMTPolicy --smtLQPolicy=AdaptiveSMTPolicy --smtIssuePolicy=Adaptive --smtCommitPolicy=Adaptive --smtAdaptiveInterval=100000 --smtAdaptiveLimit=0.8'
     ;;
-  symmetric)
-#    outdir=symmetric
-    simopts='--smtFetchPolicy=Adaptive --smtBTBPolicy=AdaptiveSMTPolicy --smtCachePolicy=AdaptiveSMTPolicy  --smtROBPolicy=AdaptiveSMTPolicy --smtIQPolicy=AdaptiveSMTPolicy --smtTLBPolicy=AdaptiveSMTPolicy --smtPhysRegPolicy=AdaptiveSMTPolicy --smtSQPolicy=AdaptiveSMTPolicy --smtLQPolicy=AdaptiveSMTPolicy  --smtIssuePolicy=Asymmetric --smtCommitPolicy=Asymmetric--smtAdaptiveInterval=100000 --smtAdaptiveLimit=0.9'
-    ;;
-  symmetric2)
-#    outdir=symmetric
-    simopts='--smtFetchPolicy=Adaptive --smtBTBPolicy=AsymmetricSMTPolicy --smtCachePolicy=AdaptiveSMTPolicy  --smtROBPolicy=AdaptiveSMTPolicy --smtIQPolicy=AdaptiveSMTPolicy --smtTLBPolicy=AdaptiveSMTPolicy --smtPhysRegPolicy=AdaptiveSMTPolicy --smtSQPolicy=AdaptiveSMTPolicy  --smtLQPolicy=AdaptiveSMTPolicy  --smtIssuePolicy=Asymmetric --smtCommitPolicy=Asymmetric --smtAdaptiveInterval=100000 --smtAdaptiveLimit=0.9'
-    ;;
-  Csymmetric3)
-#    outdir=symmetric
-    simopts='--smtFetchPolicy=Adaptive --smtBTBPolicy=AsymmetricSMTPolicy --smtCachePolicy=AsymmetricSMTPolicy  --smtROBPolicy=AdaptiveSMTPolicy --smtIQPolicy=AdaptiveSMTPolicy --smtTLBPolicy=AdaptiveSMTPolicy --smtPhysRegPolicy=AdaptiveSMTPolicy --smtSQPolicy=AdaptiveSMTPolicy  --smtLQPolicy=AdaptiveSMTPolicy  --smtIssuePolicy=Asymmetric --smtCommitPolicy=Asymmetric --smtAdaptiveInterval=100000 --smtAdaptiveLimit=0.9'
-    ;;
   asymmetric)
-#    outdir=symmetric
     simopts='--smtFetchPolicy=Asymmetric --smtBTBPolicy=AsymmetricSMTPolicy --smtCachePolicy=AsymmetricSMTPolicy  --smtROBPolicy=AdaptiveSMTPolicy --smtIQPolicy=AdaptiveSMTPolicy --smtTLBPolicy=AdaptiveSMTPolicy --smtPhysRegPolicy=AdaptiveSMTPolicy --smtSQPolicy=AdaptiveSMTPolicy  --smtLQPolicy=AdaptiveSMTPolicy  --smtIssuePolicy=Asymmetric --smtCommitPolicy=Asymmetric  --smtAdaptiveInterval=100000 --smtAdaptiveLimit=0.9'
     ;;
   *)
@@ -56,8 +41,9 @@ case "$1" in
     exit
 esac
 
-outdir=$1__$2__$3
+outdir=results/$1__$2__$3
 
+cd $WORKLOAD
 echo running scenario $1
 echo running: $bin1 $args1 xx $bin2 $args2
 echo 
@@ -66,8 +52,7 @@ echo $simopts
 
 mkdir $outdir
 mkdir $outdir/checkpoints
-# simulate for some 6~8 hours and take 100 checkpoints over that time
-$PROJ/gem5-smt/build/X86/gem5.fast  --outdir $outdir --stats-file=stat-file.txt $PROJ/gem5-smt/configs/example/se.py -c "$bin2;$bin1" --options="$args2;$args1" -I 100000000 --caches --cpu-type=Skylake_3 --l2cache --l1d_size=32kB --l1d_assoc=8 --l1i_size=32kB --l1i_assoc=8  --smt $simopts --maxinsts_threadID=0
+$PROJ/gem5/build/X86/gem5.fast  --outdir $outdir --stats-file=stat-file.txt $PROJ/gem5/configs/example/se.py -c "$bin2;$bin1" --options="$args2;$args1" -I 100000000 --caches --cpu-type=Skylake_3 --l2cache --l1d_size=32kB --l1d_assoc=8 --l1i_size=32kB --l1i_assoc=8  --smt $simopts --maxinsts_threadID=0
 echo $PROJ
 
 echo done running: $bin1 $args1 XXX  $bin2 $args2
--- mitigations/scripts/browserlike/runall.sh
+++ mitigations/scripts/browserlike/runall.sh
@@ -1,5 +1,8 @@
 # format: run.sh type cryptotype jstype
 #for type in adaptive partitioned symmetric shared;
+
+BASE=`dirname "$0"`
+mkdir -p logs
 for type in  adaptive asymmetric partitioned shared;
 do
   for cryptotype in -aes-gcm -rsa;
@@ -7,7 +10,7 @@ do
     for jstype in 3d-raytrace.js controlflow-recursive.js math-cordic.js regexp-dna.js string-base64.js;
     do
       echo starting $type $cryptotype $jstype
-      ./run.sh $type $cryptotype $jstype 2&>1 > ${type}__${cryptotype}__${jstype}.stdoutsderr &
+      $BASE/run.sh $type $cryptotype $jstype 2&>1 > logs/${type}__${cryptotype}__${jstype}.stdoutsderr &
     done
   done
 done
